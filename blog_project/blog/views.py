5d5436c34e3c05b1d55f9fb31524bd582990015a
Harrishow <harrisonquixabeira@gmail.com>
2024-03-15 22:43:45 -0300

    Primeira versão do blog

---
+++ blog_project/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_details(request):
+    post = Post.objects.all()
+    return render(request, 'blog/post_details.html', {'post': post})
