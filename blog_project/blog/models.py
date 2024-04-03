4e1a7a6fd477e8bef113c94233f916781b3c0c6e
Harrishow <harrisonquixabeira@gmail.com>
2024-04-03 07:22:52 -0300

    Refazendo o código do blog

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -8,7 +8,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    content = models.CharField(max_lenth=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(Post, on_delete=models.PROTECT)
--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('details', views.post_details, name='details'),
-    path('about', views.about, name='about'),
+
 ]
